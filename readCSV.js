e7122278ecd93a3ad3787e5042c300dd4d098149
TarCode <taariq@projectcodex.co>
2015-11-24 10:55:44 +0200

    read csv test and function

---
+++ readCSV.js
@@ -0,0 +1,22 @@
+
+var fs = require('fs');
+
+exports.readCSV = function(fileName) {
+    var stream = fs.readFileSync(fileName);
+
+   var list = stream.toString();
+  var rows = (list.split('\r')).slice(1);
+
+    var itemisedBill = rows.map(function(row){
+        var fields = row.split(',');
+            return{
+                date: fields[0],
+                provider: fields[1],
+                number: fields[2],
+                duration: fields[3]
+            }
+    });
+
+    return itemisedBill;
+
+}
---
+++ test/readCsvTest.js
@@ -0,0 +1,10 @@
+var readCSV = require('../readCSV');
+var assert = require("assert");
+
+describe('Read Itemised Bill CSV', function(){
+    /** judges comp list test **/
+    it('should read CSV file and return a list of object maps', function (done) {
+      assert.equal(readCSV.readCSV("./ItemisedBill.csv").length, 36);
+      done();
+    });
+});
