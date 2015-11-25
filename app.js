8843ed81afe67dcb03d886e182ea5d03276d1fc7
TarCode <taariq@projectcodex.co>
2015-11-25 15:24:04 +0200

    added handlebars template with data

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require('express'),
+    exphbs = require('express-handlebars'),
+    bodyParser = require('body-parser'),
+    app = express()
+    readCSV = require('./methods/readCSV'),
+    getPhoneCalls = require("./methods/getPhoneCalls");
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+var bill = readCSV.readCSV('./ItemisedBill.csv');
+var mtn = getPhoneCalls.getPhoneCalls(bill, "MTN");
+var vodacom = getPhoneCalls.getPhoneCalls(bill, "Vodacom");
+var cellc = getPhoneCalls.getPhoneCalls(bill, "CellC");
+
+
+app.get('/', function(req, res){
+  res.render('home', {all: bill.length, mtn: mtn.length, vodacom : vodacom.length, cellc : cellc.length});
+});
+
+app.get('/all', function(req, res){
+  res.render('all', {bill: bill, all: bill.length, mtn: mtn.length, vodacom : vodacom.length, cellc : cellc.length});
+});
+
+app.get('/mtn', function(req, res){
+  res.render('provider', {bill: mtn, provider : "MTN", all: bill.length, mtn: mtn.length, vodacom : vodacom.length, cellc : cellc.length});
+});
+
+app.get('/vodacom', function(req, res){
+  res.render('provider', {bill: vodacom, provider: "Vodacom", all: bill.length, mtn: mtn.length, vodacom : vodacom.length, cellc : cellc.length});
+});
+
+app.get('/cellc', function(req, res){
+  res.render('provider', {bill: cellc, provider: "Cell C", all: bill.length, mtn: mtn.length, vodacom : vodacom.length, cellc : cellc.length});
+});
+
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function(){
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('App listening at http://%s:%s', host, port);
+});
